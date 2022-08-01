db6e00416b8648ba741335932bdc2ac2159aee34
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-07-31 23:47:18 -0400

    add 'belongsToMany' links for Product and Tag, reset database, write 'get all products' route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    order: ['category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag, through: ProductTag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
